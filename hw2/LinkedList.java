909b01396eb71cc740f167eb05bf5f2217b36cb6
shengitcode <46137960+shengitcode@users.noreply.github.com>
2018-12-26 15:37:49 -0500

    Add files via upload

---
+++ hw2/LinkedList.java
@@ -0,0 +1,356 @@
+
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class LinkedList<T> implements Iterable<T>{
+	private Node<T> head;
+	
+	 /**
+	   *  Constructs an empty list
+	   */
+	public LinkedList() {
+		this.head = null;
+	}
+	
+	 /**
+	   *  Returns size of the list
+	   */
+	public int size() {
+		int size = 0;
+		if(head != null) {
+			size ++;
+			Node<T> current = head;
+			while(current.next!= null) {
+				size ++;
+				current = current.next;
+			}
+		}
+		
+		return size;
+	}
+	
+	 /**
+	   *  Returns true if the list is empty
+	   *
+	   */
+	public boolean isEmpty() {
+		return head == null;
+	}
+	
+	 /**
+	   *  Removes all nodes from the list.
+	   *
+	   */
+	public void clear() {
+		this.head = null;
+	}
+	
+	 /**
+	   *  Returns the data at the specified position in the list.
+	   */
+	public T get(int pos) {
+		if(head == null) throw new IndexOutOfBoundsException();
+		int currentIndex = 0;
+		Node<T> current = head;
+		while(currentIndex < pos && current!= null) {
+			currentIndex ++;
+			current = current.next;
+		}
+		
+		if(current != null) {
+			return current.data;
+		} else{
+			throw new IndexOutOfBoundsException();
+		}
+		
+	}
+	
+	/**
+	   *  Returns true if this list contains the specified element.
+	   *
+	   */
+	public boolean contains(T x) {
+		for(T temp: this) {
+			if(temp.equals(x)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	/**
+	  *  Returns the first element in the list.
+	  *
+	  */
+	public T getFirst() {
+		if(head == null) throw new NoSuchElementException();
+		return head.data;
+	}
+	
+	 /**
+	   *  Inserts a new node at the beginning of this list.
+	   *
+	   */
+	public void addToFirst(T data) {
+		this.head = new Node<T>(data, head);
+	}
+	
+	/**
+	  *  Removes the first element in the list.
+	  *
+	  */
+	public T removeFirst() {
+		T temp = getFirst();
+		head = head.next;
+		return temp;
+	}
+	
+	/**
+	   *  Returns the last element in the list.
+	   *
+	   */
+	public T getLast() {
+		if(head == null)  throw new NoSuchElementException();
+		Node<T> current = head;
+		while(current.next != null) {
+			current = current.next;
+		}
+		return current.data;
+	}
+	
+	/**
+	   *  Inserts a new node to the end of this list.
+	   *
+	   */
+	public void addToLast(T data) {
+		if(head == null) {
+			head = new Node<T>(data, head);
+		} else{
+			Node<T> current = head;
+			while(current.next != null) {
+				current = current.next;
+			}
+			current.next = new Node<T>(data, null);
+		}
+	}
+	
+	
+	/**
+	 * Removes the first occurrence of the specified element in this list.
+	 * 
+	 */
+	
+	public void remove(T key) {
+		if(head == null) throw new NoSuchElementException();
+		if(head.data.equals(key)) {
+			head = head.next;
+			return;
+		}
+		
+		Node<T> prev = null;
+		Node<T> current = head;
+		while(current != null && !current.data.equals(key)) {
+			prev = current;
+			current = current.next;
+		}
+		if(current != null) {
+			prev.next = current.next;
+		} else{
+			throw new NoSuchElementException();
+		}
+	}
+	
+	
+	
+	/**
+	 * The Node class
+	 */
+	private static class Node<T> {
+		private T data;
+		private Node<T> next;
+		
+		public Node(T data, Node<T> next) {
+			this.data = data;
+			this.next = next;
+		}
+	}
+	
+	/**
+	 * 
+	 * The Iterator class
+	 */
+	
+	@Override
+	public Iterator<T> iterator() {
+		return new LinkedListIterator();
+	}
+	
+	private class LinkedListIterator implements Iterator<T> {
+		private Node<T> nextNode;
+		
+		public LinkedListIterator() {
+			this.nextNode = head;
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return nextNode != null;
+		}
+
+		@Override
+		public T next() {
+			if(!hasNext()) throw new NoSuchElementException();
+			T d = nextNode.data;
+			nextNode = nextNode.next;
+			return d;
+		}
+		
+	}
+	
+	 /**
+	   *  Returns a string representation
+	   *
+	   */
+	@Override
+	public String toString() {
+		StringBuffer result = new StringBuffer();
+	      for(Object x : this)
+	      	result.append(x + " ");
+
+	      return result.toString();
+	}
+	
+	/*
+	 * *************************
+	 * Homework Problems
+	 * 
+	 * *************************
+	 */
+	
+	/**
+	 *  Inserts a new node at pos.
+	 */
+	public void insertAt(int pos, T toInsert) {
+        if(pos<0) throw new IllegalArgumentException();
+        if(head == null) throw new IndexOutOfBoundsException();
+        int currentIndex = 1;
+        Node<T> current = head;
+        while(currentIndex < pos && current!= null) {
+            currentIndex ++;
+            current = current.next;
+        }
+        if(current != null) {
+            Node<T> insertNode=new Node<T>(toInsert,current.next);
+            current.next=insertNode;
+        } else{
+            throw new IndexOutOfBoundsException();
+        }
+        
+	}
+	
+	/**
+	 * Removes the item at position pos in this list.
+	 * 
+	 */
+	public void removeAt(int pos) {
+        if(pos<0) throw new IllegalArgumentException();
+        if(head == null) throw new IndexOutOfBoundsException();
+        int currentIndex = 1;
+        Node<T> current = head;
+        while(currentIndex < pos && current!= null) {
+            currentIndex ++;
+            current = current.next;
+        }
+        if(current != null) {
+            Node<T> removeNode=current.next;
+            current.next=removeNode.next;
+        } else{
+            throw new IndexOutOfBoundsException();
+        }
+	}
+	
+	/**
+	 *  Inserts a new node after a node containing the key.
+	 *
+	 */
+	public void insertAfter(T key, T toInsert) {
+        if(head == null) throw new IndexOutOfBoundsException();
+        if(!contains(key)) throw new IndexOutOfBoundsException();
+        Node<T> current = head;
+        while(key!=current.data && current!= null) {
+            current = current.next;
+        }
+        if(current != null && key==current.data) {
+            Node<T> insertNode = new Node<T>(toInsert,current.next);
+            current.next=insertNode;
+        } else{
+            throw new IndexOutOfBoundsException();
+        }
+	}
+	
+	 /**
+	   *  Inserts a new node before a node containing the key.
+	   */
+	public void insertBefore(T key, T toInsert) {
+        if(head == null) throw new IndexOutOfBoundsException();
+        if(!contains(key)) throw new IndexOutOfBoundsException();
+        Node<T> current = head;
+        while(key!=current.data && current!= null) {
+            if(current.next.data==key){
+                Node<T> insertNode=current.next;
+                current.next=new Node<T>(toInsert,insertNode);
+                break;
+            }
+            current=current.next;
+        }
+        if(current != null) {
+            if(key==current.data){
+                head=new Node<T>(toInsert,head);
+            }
+        } else{
+            throw new IndexOutOfBoundsException();
+        }
+
+	}
+	
+	/**
+	  *  sort the list
+	  */
+	public void sortList() {
+        int nsize=size();
+        for(int i=0;i<nsize;i++){
+           for(int j=i+1;j<nsize;j++){
+               if((Integer)get(i)>(Integer)get(j)){
+                   T tempData1=get(i);
+                   T tempData2=get(j);
+                   insertAt(i,tempData2);
+                   insertAt(j+1,tempData1);
+                   removeAt(i+1);   //224624345
+                                    //22624345
+                   removeAt(j+1);
+                   
+               }
+           }
+        }
+	}
+	
+	/**
+	 * removes duplicate elements from the list
+	 */
+	public void removeDuplicates() {
+        Node<T> current = head;
+        int nsize=size();
+        for(int i=0;i<nsize;i++){
+            T tempData=current.data;
+            remove(current.data);
+            current=current.next;
+            if(!contains(tempData)){
+                addToLast(tempData);
+            }
+        }
+	}
+
+	
+}
---
+++ hw2/LinkedListTest.java
@@ -0,0 +1,199 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LinkedListTest {
+
+	private static final int INSERT_AT = 1;
+	private static final int REMOVE_AT = 2;
+	private static final int INSERT_AFTER = 3;
+	private static final int INSERT_BEFORE = 4;
+	private static final int SORT_LIST = 5;
+	private static final int REMOVE_DUPLICATES = 6;
+	
+	public static void main(String[] args) {
+		
+		try {
+			String sCurrentLine;
+			LinkedList<Integer> list1 = null;
+			LinkedList<Integer> list2 = null;
+			
+			// read and test first input file and create list1
+			BufferedReader br = new BufferedReader(new FileReader("input1.txt"));
+			int currentIndex = 0;
+			System.out.println("reading input file: input1.txt");
+			while ((sCurrentLine = br.readLine()) != null) {
+				if(currentIndex == 0) {// read elements 
+					list1 = createListFromInput(sCurrentLine);	
+					System.out.println("List: " + list1.toString());
+				} else { // read commands
+					String[] command = sCurrentLine.split(" ");
+					int commandCode = Integer.parseInt(command[0]);
+					
+					switch(commandCode) {
+						case INSERT_AT:
+							int pos = Integer.parseInt(command[1]);
+							int data = Integer.parseInt(command[2]);
+							list1.insertAt(pos,data);
+							System.out.println("insertAt (" + pos + ", " + data +")");
+							System.out.println("List: " + list1.toString());
+							break;
+						case REMOVE_AT:
+							pos = Integer.parseInt(command[1]);
+							list1.removeAt(pos);
+							System.out.println("removeAt (" + pos + ")");
+							System.out.println("List: " + list1.toString());
+							break;
+						case INSERT_AFTER:
+							int key = Integer.parseInt(command[1]);
+							data = Integer.parseInt(command[2]);
+							list1.insertAfter(key,data);
+							System.out.println("insertAfter (" + key + ", " + data +")");
+							System.out.println("List: " + list1.toString());
+							break;
+						case INSERT_BEFORE:
+							key = Integer.parseInt(command[1]);
+							data = Integer.parseInt(command[2]);
+							list1.insertBefore(key,data);
+							System.out.println("insertBefore (" + key + ", " + data +")");
+							System.out.println("List: " + list1.toString());
+							break;
+						case SORT_LIST:
+							list1.sortList();
+							System.out.println("sortList()");
+							System.out.println("List: " + list1.toString());
+							break;
+						case REMOVE_DUPLICATES:
+							list1.removeDuplicates();
+							System.out.println("removeDuplicates()");
+							System.out.println("List: " + list1.toString());
+							break;
+						default:
+					}
+					
+					
+				}
+				currentIndex ++;
+			}
+			br.close();
+			
+			System.out.println();
+			// read and test second input file and create list1
+			br = new BufferedReader(new FileReader("input2.txt"));
+			currentIndex = 0;
+			System.out.println("reading input file: input2.txt");
+			while ((sCurrentLine = br.readLine()) != null) {
+				if(currentIndex == 0) {// read elements 
+					list2 = createListFromInput(sCurrentLine);	
+					System.out.println("List: " + list2.toString());
+				} else { // read commands
+					String[] command = sCurrentLine.split(" ");
+					int commandCode = Integer.parseInt(command[0]);
+					
+					switch(commandCode) {
+						case INSERT_AT:
+							int pos = Integer.parseInt(command[1]);
+							int data = Integer.parseInt(command[2]);
+							list2.insertAt(pos,data);
+							System.out.println("insertAt (" + pos + ", " + data +")");
+							System.out.println("List: " + list2.toString());
+							break;
+						case REMOVE_AT:
+							pos = Integer.parseInt(command[1]);
+							list2.removeAt(pos);
+							System.out.println("removeAt (" + pos + ")");
+							System.out.println("List: " + list2.toString());
+							break;
+						case INSERT_AFTER:
+							int key = Integer.parseInt(command[1]);
+							data = Integer.parseInt(command[2]);
+							list2.insertAfter(key,data);
+							System.out.println("insertAfter (" + key + ", " + data +")");
+							System.out.println("List: " + list2.toString());
+							break;
+						case INSERT_BEFORE:
+							key = Integer.parseInt(command[1]);
+							data = Integer.parseInt(command[2]);
+							list2.insertBefore(key,data);
+							System.out.println("insertBefore (" + key + ", " + data +")");
+							System.out.println("List: " + list2.toString());
+							break;
+						case SORT_LIST:
+							list2.sortList();
+							System.out.println("sortList()");
+							System.out.println("List: " + list2.toString());
+							break;
+						case REMOVE_DUPLICATES:
+							list2.removeDuplicates();
+							System.out.println("removeDuplicates()");
+							System.out.println("List: " + list2.toString());
+							break;
+						default:
+					}
+					
+				}
+				currentIndex ++;
+			}
+			br.close();
+			
+			System.out.println();
+			// merge 2 lists
+			if(list1 != null && list2 != null) {
+				System.out.println("List 1: " + list1.toString());
+				System.out.println("List 2: " + list2.toString());
+				LinkedList<Integer> output = mergeLists(list1, list2);
+				System.out.println("Output: " + output.toString());
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	/**
+	 * 
+	 * Implement a mergeLists method in LinkedListTest class that merges 2 given
+	 * lists and returns an output list. While merging make sure: 1. Output list is
+	 * sorted 2. Remove any duplicate
+	 * 
+	 */
+
+	public static LinkedList<Integer> mergeLists(LinkedList<Integer> list1, LinkedList<Integer> list2) {
+		LinkedList<Integer> output = new LinkedList<Integer>();
+		/**
+		 * Your code goes here
+		 */
+        while(!list1.isEmpty()){
+            output.addToLast(list1.getFirst());
+            list1.removeFirst();
+        }
+        while(!list2.isEmpty()){
+            output.addToLast(list2.getFirst());
+            list2.removeFirst();
+        }
+        output.sortList();
+        output.removeDuplicates();
+
+		return output;
+
+	}
+	
+	public static LinkedList<Integer> createListFromInput(String input) {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		if(input != null) {
+			String[] numbers = input.split(" ");
+			for(String number:numbers) {
+				list.addToLast(Integer.parseInt(number));
+			}
+		}
+		return list;
+		
+	}
+
+}
